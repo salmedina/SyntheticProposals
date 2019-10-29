f8bd8f65e007dcb5386c91911d08cc972a37b5cc
salmedina <salbed.medina@gmail.com>
2019-10-29 13:35:26 -0400

    Check all the proposals and verify how many are missing

--- src/analysis/analyze_virat_phone.py
+++ src/analysis/analyze_virat_phone.py
@@ -11,12 +11,11 @@ virat_anno = EasyDict(json.load(open(virat_anno_path, 'r')))
 count = defaultdict(int)
 missing_count = defaultdict(int)
 for name, value in virat_anno.database.items():
-    if 'phone' in value.annotations.label:
-        print(f'{name},{value.subset},{value.annotations.label}')
-        count[value.subset] += 1
+    print(f'{name},{value.subset},{value.annotations.label}')
+    count[value.subset] += 1
 
-        if not osp.exists(osp.join(virat_dataset_dir, f'{name}')):
-            missing_count[value.subset] += 1
+    if not osp.exists(osp.join(virat_dataset_dir, f'{name}')):
+        missing_count[value.subset] += 1
 
 print('Annotation count')
 for k, v in count.items():
